a27e160a3e70bd36f8b394d4038f31e8f80e7e58
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-06-12 11:09:15 -0400

    initial configs commit

---
+++ painel_administrativo-front/src/components/layout/footer.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils"
+
+import { buttonVariants } from "@/components/ui/button"
+
+export function Footer() {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="bg-background border-t border-sidebar-border">
+      <div className="container flex justify-between items-center p-4 md:px-6">
+        <p className="text-xs text-muted-foreground md:text-sm">
+          © {currentYear}{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
+          >
+            Norte Auto Pecas
+          </a>
+          .
+        </p>
+        <p className="text-xs text-muted-foreground md:text-sm">
+          Projetado e desenvolvido por{" "}
+          <a
+            href="https://www.linkedin.com/in/oswaldo-mauricio/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
+          >
+            Oswaldo Mauricio
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  )
+}
