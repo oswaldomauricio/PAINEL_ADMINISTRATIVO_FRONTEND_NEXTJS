6b6b558513568f0fdec90cdc2ec83adcda5db5cd
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-06-15 20:55:22 -0400

    style: changing the colors of components and pages

--- painel_administrativo-front/src/app/(modulos)/(garantia)/page.tsx
+++ painel_administrativo-front/src/app/(modulos)/(garantia)/page.tsx
@@ -10,14 +10,14 @@ export default function HomePage() {
     <div className="container">
       <div className="p-4 flex flex-row justify-start gap-4">
         <Button
-          className="bg-secondary text-white hover:bg-background focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 rounded-md px-4 py-2 h-60 w-60 flex flex-col items-center justify-center gap-y-2"
+          className="bg-foreground text-white hover:bg-primary focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 rounded-md px-4 py-2 h-60 w-60 flex flex-col items-center justify-center gap-y-2"
           onClick={() => redirect("/garantias")}
         >
           <Wrench className="me-2 size-20" aria-hidden />
           <p className="text-xl">GARANTIAS</p>
         </Button>
         <Button
-          className="bg-secondary text-white hover:bg-background focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 rounded-md px-4 py-2 h-60 w-60 flex flex-col items-center justify-center gap-y-2"
+          className="bg-foreground text-white hover:bg-primary focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 rounded-md px-4 py-2 h-60 w-60 flex flex-col items-center justify-center gap-y-2"
           onClick={() => redirect("/divergencias")}
         >
           <Package className="me-2 size-20" aria-hidden />
