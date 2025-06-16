3a6f57cdc4563218996ffe0e4fbf9d9e7cb43490
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-06-15 20:58:35 -0400

    feat: create the type and datas to divergence

---
+++ painel_administrativo-front/src/app/(modulos)/types/types.ts
@@ -0,0 +1,10 @@
+export interface divergenciasType {
+  id: string
+  store: string // loja
+  date: Date // data
+  days_remaining: number // dias restantes
+  supplier: string // fornecedor
+  sales_invoice: string // nota fiscal de venda
+  status: "Pendente" | "Aprovada" | "Rejeitada" // status
+  description: string // descrição
+}
