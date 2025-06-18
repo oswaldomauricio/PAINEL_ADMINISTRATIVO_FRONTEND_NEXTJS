bcfcd6d07694cb7580b8dd248c0a277f2293ab82
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-06-18 14:05:36 -0400

    feat: componente de upload

--- painel_administrativo-front/src/app/components/upload.tsx
+++ painel_administrativo-front/src/app/components/upload.tsx
@@ -6,33 +6,49 @@ import type { UploadProps } from "antd"
 
 const { Dragger } = Upload
 
-const props: UploadProps = {
-  name: "file",
-  multiple: true,
-  action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload", //trocar para a api que faz upload de arquivo.
-  onChange(info) {
-    const { status } = info.file
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList)
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`)
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`)
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files)
-  },
+type UploadComponentProps = {
+  height?: number
+  iconSize?: number
+  text?: boolean
 }
 
-const UploadComponent: React.FC = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Clique ou arraste aqui para fazer o envio</p>
-  </Dragger>
-)
+const UploadComponent: React.FC<UploadComponentProps> = ({
+  height = 180,
+  iconSize = 48,
+  text = true,
+}) => {
+  const props: UploadProps = {
+    name: "file",
+    multiple: true,
+    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload", //trocar para a api que faz upload de arquivo.
+    onChange(info) {
+      const { status } = info.file
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList)
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} Arquivo enviado com sucesso.`)
+      } else if (status === "error") {
+        message.error(`${info.file.name} Falha ao carregar arquivo!`)
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files)
+    },
+  }
+
+  return (
+    <Dragger {...props} height={height}>
+      <p className="ant-upload-drag-icon" style={{ fontSize: iconSize }}>
+        <InboxOutlined style={{ fontSize: iconSize }} />
+      </p>
+      {text && (
+        <p className="ant-upload-text">
+          Clique ou arraste aqui para fazer o envio
+        </p>
+      )}
+    </Dragger>
+  )
+}
 
 export default UploadComponent
