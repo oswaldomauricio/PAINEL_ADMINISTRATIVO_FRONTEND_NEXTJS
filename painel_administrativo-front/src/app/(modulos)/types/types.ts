7a056da0a36340fac38de30d651f705934a8996c
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-06-19 11:34:28 -0400

    raw: adjusting the divergence and guarantee typing, and adding new data to the divergence.

--- painel_administrativo-front/src/app/(modulos)/types/types.ts
+++ painel_administrativo-front/src/app/(modulos)/types/types.ts
@@ -1,11 +1,20 @@
 export interface divergenciasType {
   id: string
   store: string // loja
-  date: Date // data
-  days_remaining: number // dias restantes
   supplier: string // fornecedor
-  salesNote: string // nota fiscal de venda
-  status: "Pendente" | "Aprovada" | "Rejeitada" // status
+  EntryDate: Date // data
+  supplierDocument: string //cnpj do fornecedor
+  openDays: number // dias restantes
+  status: "NOVO" | "PENDENTE" | "APROVADO" // status
+  produtos: [
+    {
+      productCode: string
+      quantity: number
+      value: number
+      type: "FALTA" | "SOBRA" | "AVARIA" | "OUTRO"
+      entryNote: number
+    },
+  ]
   description: string // descrição
 }
 
