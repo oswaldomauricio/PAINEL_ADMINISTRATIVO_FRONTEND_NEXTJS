463108abcefdddc7d0dba0b1b187edab4c5edcef
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-06-24 12:13:11 -0400

    feat: add getStatusColor function to return color classes based on status

--- painel_administrativo-front/src/lib/utils.ts
+++ painel_administrativo-front/src/lib/utils.ts
@@ -352,3 +352,16 @@ export function getDictionaryValue(
 
   return value
 }
+
+export const getStatusColor = (status: string) => {
+  switch (status.toLowerCase()) {
+    case "NOVO":
+      return "bg-red-100 text-red-800"
+    case "PENDENTE":
+      return "bg-yellow-100 text-yellow-800"
+    case "RESOLVIDO":
+      return "bg-green-100 text-green-800"
+    default:
+      return "bg-gray-100 text-gray-800"
+  }
+}
